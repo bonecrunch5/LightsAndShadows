2a369a941e20171b0b1a4a70fe8670ebcb260dbe
Octanas <filipe.g.barbosa@gmail.com>
2022-01-09 12:51:15 +0000

    refactor wall shadow detection

--- Assets/ShadowDetection.cs
+++ Assets/ShadowDetection.cs
@@ -10,6 +10,12 @@ public class ShadowDetection : MonoBehaviour
 
     private MeshFilter meshFilter;
 
+    private int numCorrectSectors = 0;
+    private int numWrongSectors = 0;
+
+    private int currentSector = 0;
+    public int sectorsPerFrame = 10;
+
     public int numHorizSectors = 10;
     public int numVertSectors = 10;
 
@@ -26,6 +32,8 @@ public class ShadowDetection : MonoBehaviour
 
     void Update()
     {
+        int numSectorsInFrame = 0;
+
         Bounds bounds = meshFilter.mesh.bounds;
 
         Vector3 planeCenter = bounds.center;
@@ -44,92 +52,104 @@ public class ShadowDetection : MonoBehaviour
         int widthJump = Mathf.FloorToInt(shadowMap.width / numHorizSectors);
         int heightJump = Mathf.FloorToInt(shadowMap.height / numVertSectors);
 
-        int numCorrectSectors = 0;
-        int numWrongSectors = 0;
-
-        for (int i = 1; i <= numVertSectors; i++)
+        if (currentSector == 0)
         {
-            Vector3 leftPoint = Vector3.Lerp(planeTopLeft, planeBottomLeft, (i - 0.5f) / numVertSectors);
-            Vector3 rightPoint = Vector3.Lerp(planeTopRight, planeBottomRight, (i - 0.5f) / numVertSectors);
-
-            for (int j = 1; j <= numHorizSectors; j++)
-            {
-                Vector3 sectorCenter = Vector3.Lerp(leftPoint, rightPoint, (j - 0.5f) / numHorizSectors);
-
-                Vector3 direction = spotlight.transform.position - sectorCenter;
-
-                float angle = Vector3.Angle(spotlight.transform.forward, -direction);
-
-                bool isShadow = true;
-                bool isInsideLightCone = true;
-
-                if (angle <= spotlight.spotAngle / 2)
-                {
-                    bool hit = Physics.Raycast(sectorCenter, direction, direction.magnitude, LayerMask.GetMask("Default"));
-
-                    if (hit)
-                    {
-                        if (debugShadows)
-                            Debug.DrawRay(sectorCenter, direction, Color.red);
-                    }
-                    else
-                        isShadow = false;
-                }
-                else
-                {
-                    isInsideLightCone = false;
-                    if (debugShadows)
-                        Debug.DrawRay(sectorCenter, direction, Color.red);
-                }
-
-                Color[] shadowMapPixels = shadowMap.GetPixels((j-1) * widthJump, (numVertSectors - i) * heightJump, widthJump, heightJump, 0);
-
-                int numShadowPixels = 0;
-                int numLightPixels = 0;
-                int numWhateverPixels = 0;
-
-                foreach (Color pixel in shadowMapPixels)
-                {
-                    if (pixel.r == 1f && pixel.g == 1f && pixel.b == 1f)
-                        numLightPixels++;
-                    else if (pixel.r == 0f && pixel.g == 0f && pixel.b == 0f)
-                        numShadowPixels++;
-                    else
-                        numWhateverPixels++;
-                }
-
-                // If numWhateverPixels is higher, then it doesn't matter if is shadow or light
-                if (numWhateverPixels > numLightPixels && numWhateverPixels > numShadowPixels)
-                {
-                    if(debugTextureWhatever)
-                        Debug.DrawRay(sectorCenter, direction, Color.yellow);
-                }
-                else
-                {
-                    bool shouldBeShadow = numLightPixels <= numShadowPixels;
-
-                    if (shouldBeShadow && debugTextureShadows)
-                        Debug.DrawRay(sectorCenter, direction, Color.blue);
-
-                    if (isInsideLightCone && shouldBeShadow == isShadow)
-                    {
-                        numCorrectSectors++;
-
-                        if (shouldBeShadow) numCorrectSectors += 4;
-                    }
-                    else
-                    {
-                        numWrongSectors++;
-
-                        if (shouldBeShadow) numWrongSectors += 4;
-                    }
-                }
-            }
+            numCorrectSectors = 0;
+            numWrongSectors = 0;
         }
 
-        float correctPercentage = ((float)numCorrectSectors / (float)(numCorrectSectors + numWrongSectors)) * 100;
+        while (numSectorsInFrame < sectorsPerFrame && currentSector < numHorizSectors * numVertSectors)
+        {
+            int indexVert = currentSector / numHorizSectors;
 
-        Debug.Log(correctPercentage + "%");
+            Vector3 leftPoint = Vector3.Lerp(planeTopLeft, planeBottomLeft, (indexVert + 0.5f) / numVertSectors);
+            Vector3 rightPoint = Vector3.Lerp(planeTopRight, planeBottomRight, (indexVert + 0.5f) / numVertSectors);
+
+            int indexHoriz = currentSector % numHorizSectors;
+
+            Vector3 sectorCenter = Vector3.Lerp(leftPoint, rightPoint, (indexHoriz + 0.5f) / numHorizSectors);
+
+            Vector3 direction = spotlight.transform.position - sectorCenter;
+
+            float angle = Vector3.Angle(spotlight.transform.forward, -direction);
+
+            bool isShadow = true;
+            bool isInsideLightCone = true;
+
+            if (angle <= spotlight.spotAngle / 2)
+            {
+                bool hit = Physics.Raycast(sectorCenter, direction, direction.magnitude, LayerMask.GetMask("Default"));
+
+                if (hit)
+                {
+                    if (debugShadows)
+                        Debug.DrawRay(sectorCenter, direction, Color.red, Time.deltaTime + 0.1f);
+                }
+                else
+                    isShadow = false;
+            }
+            else
+            {
+                isInsideLightCone = false;
+                if (debugShadows)
+                    Debug.DrawRay(sectorCenter, direction, Color.red, Time.deltaTime + 0.1f);
+            }
+
+            Color[] shadowMapPixels = shadowMap.GetPixels(indexHoriz * widthJump, (numVertSectors - indexVert - 1) * heightJump, widthJump, heightJump, 0);
+
+            int numShadowPixels = 0;
+            int numLightPixels = 0;
+            int numWhateverPixels = 0;
+
+            foreach (Color pixel in shadowMapPixels)
+            {
+                if (pixel.r == 1f && pixel.g == 1f && pixel.b == 1f)
+                    numLightPixels++;
+                else if (pixel.r == 0f && pixel.g == 0f && pixel.b == 0f)
+                    numShadowPixels++;
+                else
+                    numWhateverPixels++;
+            }
+
+            // If numWhateverPixels is higher, then it doesn't matter if is shadow or light
+            if (numWhateverPixels > numLightPixels && numWhateverPixels > numShadowPixels)
+            {
+                if (debugTextureWhatever)
+                    Debug.DrawRay(sectorCenter, direction, Color.yellow, Time.deltaTime + 0.1f);
+            }
+            else
+            {
+                bool shouldBeShadow = numLightPixels <= numShadowPixels;
+
+                if (shouldBeShadow && debugTextureShadows)
+                    Debug.DrawRay(sectorCenter, direction, Color.blue, Time.deltaTime + 0.1f);
+
+                if (isInsideLightCone && shouldBeShadow == isShadow)
+                {
+                    numCorrectSectors++;
+
+                    if (shouldBeShadow) numCorrectSectors += 4;
+                }
+                else
+                {
+                    numWrongSectors++;
+
+                    if (shouldBeShadow) numWrongSectors += 4;
+                }
+            }
+
+            numSectorsInFrame++;
+            currentSector++;
+        }
+
+        if (currentSector >= numHorizSectors * numVertSectors)
+        {
+            float correctPercentage = ((float)numCorrectSectors / (float)(numCorrectSectors + numWrongSectors)) * 100;
+
+            Debug.Log(correctPercentage + "%");
+
+            currentSector = 0;
+        }
 
         // DEBUG: draw plane bounding box
         for (int i = 0; i <= numVertSectors; i++)
